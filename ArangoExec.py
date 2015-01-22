8dbbc29f41e1e54434c3ee86dc851d313ba1b143
clintwood (Office) <clint@anotherway.co.za>
2015-01-22 19:54:09 +0200

    added comment support, results to file, bindVars from comments, more settings

--- ArangoExec.py
+++ ArangoExec.py
@@ -1,16 +1,20 @@
-import sublime, sublime_plugin, http.client, socket, types, threading, time, json
+import sublime, sublime_plugin, http.client, socket, types, threading, time, json, os, re
 
 class Options:
     def __init__(self, name):
         self.name     = name
         connections   = sublime.load_settings("ArangoExec.sublime-settings").get('connections')
-        self.host     = connections[self.name]['host']
-        self.port     = connections[self.name]['port']
-        self.username = connections[self.name]['username']
-        self.password = connections[self.name]['password']
-        self.database = connections[self.name]['database']
-        if 'service' in connections[self.name]:
-            self.service  = connections[self.name]['service']
+        conn = connections[self.name]
+        self.host             = conn['host']
+        self.port             = conn['port']
+        self.username         = conn['username']
+        self.password         = conn['password']
+        self.database         = conn['database']
+        self.service          = conn['service'] if 'service' in conn else None
+        self.resultFileName   = conn['resultFileName'] if 'resultFileName' in conn else ''
+        self.autoSave         = conn['autoSave'] if 'autoSave' in conn else False
+        self.resultCount      = conn['resultCount'] if 'resultCount' in conn else True
+        self.batchSize        = conn['batchSize'] if 'batchSize' in conn else 1000
 
     def __str__(self):
         return self.name
@@ -25,6 +29,8 @@ class Options:
         return names
 
 class Command():
+    def __init__(self):
+      self._resultView = None
 
     FILE_TYPE_HTML = "html"
     FILE_TYPE_JSON = "json"
@@ -33,19 +39,26 @@ class Command():
     HTML_CHARSET_HEADER = "CHARSET"
     htmlCharset = "utf-8"
 
-    def explain(self, query):
-        self._clearConsole()
-        requestObject = { 'query' : query }
+    def explain(self, query, clear):
+        bindVars = self._bindVarsFromComments(query)
+        if bindVars:
+          requestObject = { 'query' : query, 'bindVars': bindVars }
+        else:
+          requestObject = { 'query' : query }
         urlPart = "/_api/explain"
         respBodyText = self._execute(requestObject, urlPart)
-        self._showToConsole(respBodyText)
+        self._showResult(respBodyText, clear)
 
-    def execute(self, query):
-        self._clearConsole()
-        requestObject = { 'query' : query, 'count' : True, 'batchSize' :100 }
+    def execute(self, query, clear):
+        options = self._getOptions()
+        bindVars = self._bindVarsFromComments(query)
+        if bindVars:
+          requestObject = { 'query' : query, 'bindVars': bindVars, 'count' : options.resultCount, 'batchSize': options.batchSize }
+        else:
+          requestObject = { 'query' : query, 'count' : options.resultCount, 'batchSize': options.batchSize }
         urlPart = "/_api/cursor"
         respBodyText = self._execute(requestObject, urlPart)
-        self._showToConsole(respBodyText)
+        self._showResult(respBodyText, clear)
 
     def fillDatabaseCollections(self):
         global collections
@@ -60,31 +73,88 @@ class Command():
         for collectionName in obj['result']:
             collections.append((collectionName, collectionName))
 
-    def _showToConsole(self, respBodyText):
-        obj = json.loads(respBodyText)
+    def _bindVarsFromComments(self, queryText):
+      result = re.search(r'^.*bindVars\s*:\s*(\{.*\})\s*$', queryText, re.M)
+      if result:
+        try:
+          return json.loads(result.group(1))
+        except:
+          return
+    
+    def _makeFileName(self, viewFileName, resultFileName):
+      if not viewFileName or not resultFileName:
+        return None
 
-        prettyRespBodyText = json.dumps(obj,
-                                  indent = 2,
-                                  ensure_ascii = False,
-                                  sort_keys = False,
-                                  separators = (',', ': '))
+      directory = os.path.dirname(viewFileName)
+      basename = os.path.basename(viewFileName)
+      if '*' in resultFileName:
+        return os.path.join(directory, resultFileName.replace('*', os.path.splitext(basename)[0]))
+      else:
+        return os.path.join(directory, resultFileName)
+    
+    def _showToResultFile(self, filename, prettyRespBodyText, clear):
+        if not self._resultView:
+          if not os.path.exists(filename):
+            #print('creating: ' + filename)
+            directory = os.path.dirname(filename)
+            if not os.path.exists(directory):
+              os.makedirs(directory)
+            open(filename, 'a').close()
+          
+          #print('opening(1): ' + filename)
+          self._resultView = sublime.active_window().open_file(filename)
+        
+        if self._resultView.is_loading():
+          #print('loading: ' + filename)
+          sublime.set_timeout(lambda: self._showToResultFile(filename, prettyRespBodyText, clear), 100)
+        else:
+          #print('opening(2): ' + filename)
+          self._resultView = sublime.active_window().open_file(filename)
+          options = self._getOptions()
+          
+          if clear:
+            self._resultView.run_command('select_all')
+            self._resultView.run_command('left_delete')
+          self._resultView.run_command('append', {'characters': prettyRespBodyText})
+          if options.autoSave:
+            self._resultView.run_command('save')
+          
+          # TODO: this really needs to be a context
+          self._resultView = None
 
+    def _showToConsole(self, prettyRespBodyText, clear):
         panel = sublime.active_window().get_output_panel("arango_panel_output")
         panel.set_read_only(False)
         panel.set_syntax_file("Packages/JavaScript/JSON.tmLanguage")
+        if clear:
+          panel.run_command('select_all')
+          panel.run_command('left_delete')
         panel.run_command('append', {'characters': prettyRespBodyText})
         panel.set_read_only(True)
         sublime.active_window().run_command("show_panel", {"panel": "output.arango_panel_output"})
 
-    def _clearConsole(self):
-        panel = sublime.active_window().get_output_panel("arango_panel_output")
-        panel.set_read_only(False)
-        panel.set_syntax_file("Packages/JavaScript/JSON.tmLanguage")
-        panel.run_command('append', {'characters': 'loading...'})
-        panel.set_read_only(True)
-        sublime.active_window().run_command("show_panel", {"panel": "output.arango_panel_output"})
-
-    def _execute(self, requestObject, urlPart):
+    def _showResult(self, respBodyText, clear):
+        options = self._getOptions()
+        
+        obj = json.loads(respBodyText)
+        prettyRespBodyText = json.dumps(obj,
+                                  indent = 2,
+                                  ensure_ascii = False,
+                                  sort_keys = True,
+                                  separators = (',', ': ')) + '\n\n'
+        if not options.resultFileName:
+          self._showToConsole(prettyRespBodyText, clear)
+        else:
+          active_view = sublime.active_window().active_view()
+          if not active_view:
+            return
+          filename = self._makeFileName(active_view.file_name(), options.resultFileName)
+          if not filename:
+            print('cannot create filename')
+            return
+          self._showToResultFile(filename, prettyRespBodyText, clear)
+          
+    def _getOptions(self):
         global selectedIndexOptions
 
         if selectedIndexOptions == -1 :
@@ -92,7 +162,10 @@ class Command():
 
         names = Options.list()
         options = Options(names[selectedIndexOptions])
-
+        return options
+    
+    def _execute(self, requestObject, urlPart):
+        options = self._getOptions()
         host = options.host
         port = options.port
         timeoutValue = 500
@@ -204,6 +277,7 @@ class ArangoExplainCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         global command
         Options.list()
+        clear = True
         for region in self.view.sel():
             # If no selection, use the entire file as the selection
             query = ''
@@ -212,9 +286,27 @@ class ArangoExplainCommand(sublime_plugin.TextCommand):
             else:
                 query = self.view.substr(sublime.Region(region.a, region.b))
 
-            command.explain(query)
+            command.explain(query, clear)
+            clear = False
 
 class ArangoExecCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        global command
+        Options.list()
+        clear = True
+        for region in self.view.sel():
+            # If no selection, use the entire file as the selection
+            query = ''
+            if region.empty() :
+                query = self.view.substr(sublime.Region(0, self.view.size()))
+            else:
+                query = self.view.substr(sublime.Region(region.a, region.b))
+
+            command.execute(query, clear)
+            clear = False
+
+class ArangoExecAppendCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         global command
@@ -227,7 +319,7 @@ class ArangoExecCommand(sublime_plugin.TextCommand):
             else:
                 query = self.view.substr(sublime.Region(region.a, region.b))
 
-            command.execute(query)
+            command.execute(query, False)
 
 class ArangoAutoComplete(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
